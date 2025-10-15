b8c094d7087495fba0bd341c0721be16a66b16c9
Gabriel <167794964+Gabriel-Proyects@users.noreply.github.com>
2025-10-14 18:27:41 -0600

    Update for pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true, // fuerza a Next a servir la imagen tal cual, sin procesarla
   },
+  basePath: '/PROYECTO-TESSITURA',
+  assetPrefix: '/PROYECTO-TESSITURA/',
 };
 
 export default nextConfig;
